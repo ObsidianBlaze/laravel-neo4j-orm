7454bd4268697f9ce2786338959a8b140f8327e3
ObsidianBlaze <wijojames@gmail.com>
2025-03-19 02:26:31 +0100

    test: example test to ensure package can use php unit for tests

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @covers ::Nothing
+     */
+
+    public function test_basic_functionality(): void
+    {
+        $this->assertTrue(true);
+    }
+}
